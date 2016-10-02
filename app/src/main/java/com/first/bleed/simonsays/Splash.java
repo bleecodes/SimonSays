98129c5c241145f302956ef096273fe8f9d4add3
Brian Lee <writeto.blee@gmail.com>
2016-10-02 14:42:35 -0400

    added splash

---
+++ app/src/main/java/com/first/bleed/simonsays/Splash.java
@@ -0,0 +1,25 @@
+package com.first.bleed.simonsays;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by BLEED on 10/2/2016.
+ */
+
+public class Splash extends Activity {
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+                finish();
+            }
+        }, 2000);
+    }
+}
